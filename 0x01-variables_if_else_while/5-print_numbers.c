6f5174866ad3ec07e266f38964f5fec75abff396
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-06 21:13:04 +0000

    5-Printing number for 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - function main where the programs start
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+return (0);
+} /* End of the program */
