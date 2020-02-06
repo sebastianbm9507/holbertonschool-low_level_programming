91e6e45549c9eb43407d9469a8c0a220fe8f1eaa
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-06 21:30:43 +0000

    7-Printing the alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function main where the programs start
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+} /* End of the program */
