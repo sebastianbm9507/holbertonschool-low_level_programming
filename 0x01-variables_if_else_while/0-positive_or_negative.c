d43c79a8787b94b0fe21e2b4b6d498fc7c46607a
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-06 18:21:01 +0000

    0-fixing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,11 +9,11 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-	if (n>0)
+	if (n > 0)
 {
 	printf("%d is positive\n", n);
 }
-	else if (n==0)
+	else if (n == 0)
 {
 	printf("%dn is zero\n", n);
 }
