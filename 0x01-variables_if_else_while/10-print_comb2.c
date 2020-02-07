439a832f45fb86b7afcbe7aa0a95cfb28b631b0b
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-07 02:45:09 +0000

    10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		int l;
+		for (l = 0; l < 100; l++)
+		{		
+			putchar(l / 10 + '0');
+			putchar(l % 10 + '0');
+			if (l != 99)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+		putchar('\n');
+		return (0);
+}
