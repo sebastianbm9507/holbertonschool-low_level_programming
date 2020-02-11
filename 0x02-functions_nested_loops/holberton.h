d16f5e2064f37085a7e3ff1b3d94e0631d381233
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-11 15:24:22 +0000

    adding my library

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,17 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+	int _putchar(char c);
+	void print_alphabet(void);
+	void print_alphabet_x10(void);
+	int _islower(int c);
+	int _isalpha(int c);
+	int print_sign(int n);
+	int _abs(int);
+#endif /* _SAMPLE_HEADER_H_ */
