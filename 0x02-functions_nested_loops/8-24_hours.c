ae8f7d3912a3643568f9d0769ccfeb58967535ae
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-11 20:09:02 +0000

    showing 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include"holberton.h"
+/**
+ * jack_bauer - print a sign depends of the value of c
+ *
+ * @void
+ *
+ * Return: 1 or 0
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0 ; hour < 24 ; hour ++)
+	{
+		for (minute = 0; minute <= 59 ; minute ++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute %10) + '0');
+			_putchar('\n');
+		}
+	}
+}
