e78be85d129fa3e8eb3bd2e0b33f878b7c4a6b40
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-12 03:24:00 +0000

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+  *print_to_98 - e
+  *@n: the start number
+  *
+  */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+	for (n = n ; n < 98 ; n++)
+	{
+	printf("%i, ", n);
+	}
+	}
+
+	if (n > 98)
+	{
+	for (n = n ; n > 98 ; n--)
+	{
+	printf("%i, ", n);
+	}
+	}
+	if (n == 98)
+	{
+	printf("%i", n);
+	}
+	printf("\n");
+}
