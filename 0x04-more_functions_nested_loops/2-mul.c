8d78830b6bd2d16e248d5c0718050aed63881512
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-13 18:54:59 +0000

    multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include"holberton.h"
+/**
+ * mul - multiply two integers
+ *
+ * @a: parameter int
+ * @b: parameter int
+ * Return: the value of a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
