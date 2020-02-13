0a420ab0fd7e7922e25c393d9b7d8dc62fc6a007
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-13 21:42:43 +0000

    5-more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include"holberton.h"
+/**
+ * more_numbers - print numbers from 0 to 9
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j = 0;
+
+	while (j <= 10)
+	{
+		i = 0;
+		while (i <= 14)
+		{
+			if ((i / 10) > 0)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		i++;
+		}
+	_putchar('\n');
+	j++;
+	}
+}
