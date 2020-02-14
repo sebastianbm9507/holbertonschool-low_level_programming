2f9b901d440953819796513fc7cf6c95b9ae8e40
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-14 03:51:22 +0000

    8-printing a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include"holberton.h"
+/**
+ * print_square - print a square according to a size
+ * @size: size of the square
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		for (j = 0 ; j < size ; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
