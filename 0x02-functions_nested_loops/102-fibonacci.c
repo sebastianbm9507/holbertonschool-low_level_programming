3e278d0e6a286f04128f1275c5fa4c0535b6ac1f
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-15 15:29:58 +0000

    102-prints first 50 numbers of fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include"holberton.h"
+/**
+ * main - execute
+ *
+ *
+ * Return: 0i
+ */
+void main(void)
+{
+	int inicio = 1;
+	int segundo = 2;
+	int resultado;
+	int i;
+    
+	printf("%d", inicio);
+	putchar(',');
+	putchar(' ');
+	printf("%d", segundo);
+	putchar(',');
+	putchar(' ');
+		for (i = 0 ; i  <= 49 ; i++)
+		{
+			resultado = inicio + segundo;
+			printf("%d", resultado);
+			putchar(',');
+			putchar(' ');
+			inicio = segundo;
+			segundo = resultado;
+		}
+		putchar('\n');
+}
