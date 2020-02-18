cc3c9a4d9462bc7d98910709a2888c297ab3b696
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-18 19:57:31 +0000

    1-swaping values

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,12 @@
 #include"holberton.h"
 /**
- * swap_int - swap a value 
- *
+ * swap_int - swap a value
  * Return: Always 0 (Success)
  */
 void swap_int(int *a, int *b)
 {
-    *a = 42;
-    *b = 98;
+*a = 42;
+*b = 98;
 }
-
 
 
