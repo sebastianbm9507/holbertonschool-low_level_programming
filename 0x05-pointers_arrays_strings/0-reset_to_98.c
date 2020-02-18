096dad87cb095069468c779306ecc6a2a45507f4
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-18 14:28:50 +0000

    adding readme , library and 0-exercise

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include"holberton.h"
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ *
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+void reset_to_98(int *n);
+#endif /* HOLBERTON_H */
