4d5400f2d7b981f7e08718e91ded9bf12acb6ffb
sebastian <juan.bueno01@usc.edu.co>
2020-02-19 04:10:14 +0000

    fixing

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,17 @@
 #include"holberton.h"
 /**
- * _strlen - swap a value 
+ * _strlen - swap a value
  *
  * Return: Always 0 (Success)
  */
 int _strlen(char *s)
 {
-    int counter = 0;  
-            while (s[counter] != '\0') 
-            {
-                counter++;
-            }
-        return counter;
+	int counter = 0;
+
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+	return (counter);
 }
 
