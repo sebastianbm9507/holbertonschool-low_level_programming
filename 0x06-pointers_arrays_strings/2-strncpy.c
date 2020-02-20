f67266816f49452ebf84ac36acc66b15f08f79a3
Sebastian bueno <juan.bueno01@usc.edu.co>
2020-02-20 19:18:00 +0000

    2-copying

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include"holberton.h"
+/**
+ * strncpy - copying an string
+ * @dest: pointer char parameter destination
+ * @src: source char pointer parameter
+ * @n: int parameter that determinate the end of bytes to concatenate
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+			dest[i] = '\0';
+	}
+	return (dest);
+}
