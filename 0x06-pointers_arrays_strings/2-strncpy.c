59798e543de59b1210568dd20f32134935ecaa8b
Sebastian bueno <juan.bueno01@usc.edu.co>
2020-02-20 21:00:13 +0000

    2-a function that copies a string.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,20 +1,21 @@
 #include"holberton.h"
 /**
- * strncpy - copying an string
+ * _strncat - copying an string
  * @dest: pointer char parameter destination
  * @src: source char pointer parameter
  * @n: int parameter that determinate the end of bytes to concatenate
  *
  * Return: Always 0 (Success)
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i;
-	for (i = 0; i < n && src[i] != '\0'; i++)
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 	{
 		dest[i] = src[i];
 	}
-	for ( ; i < n; i++)
+		for ( ; i < n ; i++)
 	{
 			dest[i] = '\0';
 	}
