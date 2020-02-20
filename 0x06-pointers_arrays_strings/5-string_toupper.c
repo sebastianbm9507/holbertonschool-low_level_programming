f83a34db49f2bf82c852544b17f7f7279b0c3691
Sebastian bueno <juan.bueno01@usc.edu.co>
2020-02-20 23:25:56 +0000

    5-changes all lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,8 @@
 #include"holberton.h"
 /**
  * string_toupper - reverses the content of an array of integers.
- * @a: pointer char parameter destination
  * @n: source char pointer parameter
+ * Return: return the n
  */
 char *string_toupper(char *n)
 {
@@ -14,10 +14,11 @@ char *string_toupper(char *n)
 		if (n[i] >= 97 && n[i] <= 122)
 		{
 			char temp;
+
 			temp = n[i];
 			n[i] = temp - 32;
 		}
-		i++;	
+		i++;
 	}
 	return (n);
 }
