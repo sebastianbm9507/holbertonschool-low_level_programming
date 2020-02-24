364f5e729432da4a9bd985fe533f46e235f31e54
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-24 23:14:09 +0000

    4-

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,28 @@
 #include "holberton.h"
-
 /**
- * _strpbrk -
+ * _strpbrk - 
  *
  * Return: 
  */
 char *_strpbrk(char *s, char *accept)
 {
+	int i, j;
 
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+			j++;
+		}
+		i++;
+		if (!s[i])
+		break;
+	}
+	return (0);
 }
