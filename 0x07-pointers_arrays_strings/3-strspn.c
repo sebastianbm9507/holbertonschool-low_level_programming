822fb205a8923854d7cc0ea730ebd0b9c66db7fc
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-24 22:23:44 +0000

    2-returning a match

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,34 @@
 #include "holberton.h"
 
 /**
- * _strspn -
- *
- * Return: 
+ * _strspn - function that gets the length of a prefix substring.
+ * @s: source string
+ * @accept:  string that need to match with source 
+ * Return: number of bytes matches.
  */
 unsigned int _strspn(char *s, char *accept)
 {
-
+	int bytes;
+	int i,j;
+	
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i]== accept[j])
+			{
+				bytes++;
+				break;
+			}
+			j++;
+		}
+		if (accept[j] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+	return (bytes);
 }
