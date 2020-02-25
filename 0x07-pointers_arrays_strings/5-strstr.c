f1d168a49142245cd98ae4333d02a111adedfc60
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-25 01:31:46 +0000

    5-finally

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,28 @@
 #include "holberton.h"
-
 /**
- * _strstr -
- *
- * Return: 
+ * _strstr - function that locates a substring.
+ * @haystack: array
+ * @needle: array
+ * Return: string
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int i, j;
 
+	i = 0;
+	while (haystack != 0)
+	{
+		j = 0;
+		while (needle != 0 && haystack != 0 && haystack[i] == needle[j])
+		{
+			j++;
+			i++;
+		}
+		if (needle[j] == 0)
+		{
+			return (&haystack[i]);
+		}
+		i++;
+	}
+	return (0);
 }
