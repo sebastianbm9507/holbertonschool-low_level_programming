7f3f9c48c11ca3e804455b74645ace121e1b4edf
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-25 03:55:33 +0000

    8-printing a diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,23 +1,19 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
- * print_diagsums -
- *
- * Return: 
+ * print_diagsums - print diagonal
+ * @a: array in
+ * @size: array size
  */
+
 void print_diagsums(int *a, int size)
 {
-	int i = 0;
-	int j = 0;
-	int sumadiag = 0;
+	int i, s1 = 0, s2 = 0;
 
-	for ( i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		if (i == j)
-		sumadiag += a[i][j];
+		s1 = s1 + a[i * size + i];
+		s2 = s2 + a[(size - 1) * (i + 1)];
 	}
-	
-	
-
-	printf("%d", sumadiag);
+	printf("%d, %d\n", s1, s2);
 }
