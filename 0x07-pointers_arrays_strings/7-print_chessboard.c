ad8598dac7fbe4a23149c4c61d5dbc3568ab30ff
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-25 02:19:00 +0000

    7-printing a chees board

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
-
 /**
- * print_chessboard -
- *
- * Return: 
+ * print_chessboard - function that prints the chessboard.
+ * @a: array
+ * Return: nothing
  */
 void print_chessboard(char (*a)[8])
 {
+	int row, col;
 
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+		_putchar ('\n');
+	}
 }
