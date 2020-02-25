450f1b2d147444eca37ba2640e5fd8f3df1b00f4
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-25 03:35:22 +0000

    8-diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,5 +7,17 @@
  */
 void print_diagsums(int *a, int size)
 {
+	int i = 0;
+	int j = 0;
+	int sumadiag = 0;
 
+	for ( i = 0; i < size; i++)
+	{
+		if (i == j)
+		sumadiag += a[i][j];
+	}
+	
+	
+
+	printf("%d", sumadiag);
 }
