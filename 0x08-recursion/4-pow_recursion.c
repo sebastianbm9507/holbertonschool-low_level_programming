41fd07ab10add8a48e60c382b69e5ca5c63c1208
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-02-27 01:28:14 +0000

    a function that returns the value of x raised to the power of y.

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
 
 /**
- * _pow_recursion -
- *
- * Return: 
+ * _pow_recursion - a function that returns the value of raised number
+ * @x: integer parameter num to be raysed
+ * @y: the number of raysed
+ * Return: If y is lower than 0, the function should return -1
  */
 int _pow_recursion(int x, int y)
 {
 	int res;
+
 	if (y < 0)
 	{
 		res	= -1;
@@ -18,8 +20,7 @@ int _pow_recursion(int x, int y)
 	}
 	else if (y > 0)
 	{
-		res = x * _pow_recursion (x , y - 1);
+		res = x * _pow_recursion(x, y - 1);
 	}
 	return (res);
 }
-	
