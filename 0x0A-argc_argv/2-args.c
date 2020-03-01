fb2613601da90b409cbff57eb7a0f27b2a160502
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-03-01 14:16:40 +0000

    2-printing all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - return the number of parameters
+ * @argc: int parameter with the size of argv
+ * @argv: char array of parameter passed to the program
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i = 0;
+	for ( i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	
+	return (0);
+}
