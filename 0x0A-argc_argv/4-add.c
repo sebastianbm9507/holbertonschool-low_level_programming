45c3de4b78516c47b521ad12690abc52a4900f32
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-03-01 17:13:33 +0000

    4-adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers.
+ * @argc: int parameter with the size of argv
+ * @argv: char array of parameter passed to the program
+ * Return: 1 error , 0 succesfull
+ */
+int main(int argc, char **argv)
+{
+	int  i, sum;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		/* go through the array  */
+		for (i = 1; i < argc ; i++)
+		{
+			/* obtain ascii value of the character and verify if it's a number */
+			if (*argv[i] > 47 && *argv[i] < 58)
+			{
+				sum = sum + atoi(argv[i]);
+			}
+			/* it's different of a number */
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
