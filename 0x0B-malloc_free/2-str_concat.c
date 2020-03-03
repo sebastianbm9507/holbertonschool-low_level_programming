8d49e603cee6ee22e6357ee670628c53537be618
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-03-03 18:54:54 +0000

    2-concatenate 2 strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,36 +19,31 @@ char *str_concat(char *s1, char *s2)
 	{
 	return (NULL);
 	}
-
-	if (s1 != NULL && s2 != NULL || s2 == NULL && s1 != NULL)
+	/** get the s1 lenght */
+	a = 0;
+	while (s1[a] != '\0')
+		a++;
+	/** get the s2 lenght */
+	i = 0;
+	while (s2[i] != '\0')
+		i++;
+	/** malloc to get the addres of my two string and handle with concatString*/
+	concaString = malloc(sizeof(char) * (a + i));
+	/** check if malloc fail */
+	if (concaString == NULL)
+		return (NULL);
+	/** insert s1 to my new array */
+	for (j = 0; s1[j] != '\0' ; j++)
 	{
-			/** get the s1 lenght */
-			a = 0;
-			while (s1[a] != '\0')
-				a++;
-			/** get the s2 lenght */
-			i = 0;
-			while (s2[i] != '\0')
-				i++;
-		/** malloc to get the addres of my two string and handle with concatString*/
-		concaString = malloc(sizeof(char) * (a + i));
-		/** check if malloc fail */
-			if (concaString == NULL)
-				return (NULL);
-			/** insert s1 to my new array */
-			for (j = 0; s1[j] != '\0' ; j++)
-			{
-				concaString[j] = s1[j];
-			}
-			/** insert s2 to my new array that already has s1 */
-			j--;
-			for (i = 0; s2[i] != 0 ; i++, j++)
-			{
-				concaString[j] = s2[i];
-			}
-		/** insert the null value to the end of s2 */
-		concaString[j] = '\0';
-		/** return the pointer to the concastring */
-		return (concaString);
-		}
+		concaString[j] = s1[j];
+	}
+	/** insert s2 to my new array that already has s1 */
+	for (i = 0; s2[i] != 0 ; i++, j++)
+	{
+		concaString[j] = s2[i];
+	}
+	/** insert the null value to the end of s2 */
+	concaString[j] = '\0';
+	/** return the pointer to the concastring */
+	return (concaString);
 }
