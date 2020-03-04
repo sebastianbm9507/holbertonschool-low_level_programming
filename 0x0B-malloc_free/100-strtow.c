15c2a014e36ffb4da7896cb85f4b1e5506ea4377
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-03-04 02:41:39 +0000

    function that splits a string into words.

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+* strtow - divide string
+* @str: string
+* Return: string word per word
+*/
+char **strtow(char *str)
+{
+	if (str == NULL || str == '\0')
+		return (NULL);
+}
