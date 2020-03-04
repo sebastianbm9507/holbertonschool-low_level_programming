5d9b226d8564ed29589f4ecaa4eea86d11ce01dd
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-03-04 02:53:46 +0000

    4-free grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
-
+#include <stdlib.h>
 /**
- * free_grid -
- *
- * Return: 
- */
+* free_grid - free a grid
+* @grid: grid
+* @height: height grid
+* Return: Void
+*/
 void free_grid(int **grid, int height)
 {
+	int index = 0;
 
+	while (index < height)
+	{
+		free(grid[index]);
+		index++;
+	}
+	free(grid);
 }
