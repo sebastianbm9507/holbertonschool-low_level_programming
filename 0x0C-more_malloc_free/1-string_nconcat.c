551ba2dd5f3d834734484552d69a30c16e079aa3
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-03-05 19:36:53 +0000

    1-concatenating a string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,15 +13,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *concat = NULL;
 	/**treating s2 as an empty string if is NULL */
 			if (s2 == NULL)
-			{
-				s2 = malloc(1);
 				s2 = "";
-			}
 			if (s1 == NULL)
-			{
-				s1 = malloc(1);
 				s1 = "";
-			}
 	/** get the size for s1*/
 	size1 = 0;
 	while (s1[size1] != '\0')
