ee2d9456d9ed37eb5622076d025d129e9afa588d
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-03-09 21:53:20 +0000

    adding readme and dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - Define a new type struct dog with the following elements:
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: 0 always
+ */
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+#endif
