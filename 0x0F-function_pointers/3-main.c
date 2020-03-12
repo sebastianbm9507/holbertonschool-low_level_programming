ab4880e38969377a9d26cb6a16d10f17fe77b004
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-03-12 02:26:38 +0000

    3-calculator - hot fix!

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,28 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - function that invoke
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: always 0
+ * main - Perform basics operations of a calculator
+ * @argc: number of arguments passed by command line
+ * @argv: vector of arguments passed by command line
+ * Return: Operation value
  */
 int main(int argc, char **argv)
 {
 	int param1, param2, res;
-	char *operator = argv[2];
-	/** catching the operator for argument */
-	param1 = atoi(argv[1]);
-	param2 = atoi(argv[3]);
-	if (argc != 4 || argv == NULL)
+	char *operator;
+
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	operator = argv[2];
+	param2 = atoi(argv[3]);
 	if (((*operator == '/') || (*operator == '%')) && (param2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
+	param1 = atoi(argv[1]);
 	res = (*get_op_func(operator))(param1, param2);
 	printf("%d\n", res);
 	return (0);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  *  op_add - add number
  * @a: first number
