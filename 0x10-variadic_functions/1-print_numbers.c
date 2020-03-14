a78823acad43fceca9b2339d48450ce2d50424e6
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-03-14 17:54:57 +0000

    1-printing numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_numbers - print numbers
+ * @separator: separator
+ * @n: size
+ * Return: 0 always
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	if (separator == NULL)
+		return;
+	/** start my list */
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
