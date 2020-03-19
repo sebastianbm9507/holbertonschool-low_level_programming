5f2c7b2644ae68a2d43fab5034cddc088e2637ce
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-03-19 21:57:56 +0000

    2-printing a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_list - Write to stdout the value of list_t
+ * @h: list_t argument
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int counter = 0;
+
+	/** once h-> is == NULL is false*/
+	while (h != NULL)
+	{
+		/** checking if str is null */
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			/** move to next node */
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			/** move to next node */
+			h = h->next;
+		}
+		/** store the number of nodes  */
+		counter++;
+	}
+	return (counter);
+}
