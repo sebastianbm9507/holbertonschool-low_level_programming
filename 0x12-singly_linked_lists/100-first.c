f6183313c4f0301f4fc11c280f6cfef55fe7c09e
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-03-20 02:01:25 +0000

    100-print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * premain - print before  main function
+ * Return:  void
+ */
+void __attribute__ ((constructor)) premain()
+{
+	    printf("You're beat! and yet, you must allow,\n");
+	    printf("I bore my house upon my back!\n");
+}
