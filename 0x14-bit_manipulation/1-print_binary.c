9c33f98459ef7389de280fc6180ac4246923f7d3
Sebastian Bueno <juan.bueno01@usc.edu.co>
2020-03-27 02:19:06 +0000

    1-Print binary incomplete

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number in base 10
+ * Return: Nothing
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		return;
+}
