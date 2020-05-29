0efdd9e7b695b71b146fba7d51407f0720d6c93d
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-29 19:01:35 +0000

    Set a element into table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,59 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Function that set a element into table
+ * @ht: hast table pointer
+ * @key: key of element to be inserted
+ * @value: value of element to be inserted
+ * Return: 1 on succes, 0 on failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *temp;
+	unsigned long int index;
+	int succes;
+	int size = 0;
+
+	size = ht->size;
+	index = key_index((unsigned char *)key, size);
+
+	/* No collision case */
+	temp = ht->array[index];
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = malloc(sizeof(hash_node_t));
+		if (ht->array[index] == NULL)
+			return (0);
+		ht->array[index]->key = strdup(key);
+		ht->array[index]->value = strdup(value);
+		ht->array[index]->next = NULL;
+		succes = 1;
+	}
+	else
+	{
+		/** create node to be inserted */
+		hash_node_t *new = malloc(sizeof(hash_node_t));
+
+		if (new == NULL)
+			return (0);
+		while (temp->next != NULL)
+		{
+			if (strcmp(temp->next->key, key) == 0)
+			{
+				free(temp->next->value);
+				temp->next->value = strdup(value);
+				succes = 1;
+				break;
+			}
+			temp = temp->next;
+		}
+		/* case there's no coincidence */
+		if (temp == NULL)
+		{
+			new->next = ht->array[index];
+			ht->array[index] = new;
+			succes = 1;
+		}
+	}
+	return (succes);
+}
